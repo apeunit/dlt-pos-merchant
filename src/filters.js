b902bc97c44121b89e3798fe6de7d86aefcd8eb6
Sadi Qevani <inaveq@gmail.com>
2019-03-26 15:45:59 +0100

    improve filter

--- src/filters.js
+++ src/filters.js
@@ -2,23 +2,11 @@ import BigNumber from 'bignumber.js'
 
 export default function formatUnit (base) {
   try {
-    if (base < 10 ** 16 && (base !== '0' || base !== 0)) {
-      let exp = 0
-      while (base > 9) {
-        base = base / 10
-        exp++
-      }
-      const extra = exp > 6 ? exp % 3 : exp - 6
-      base = new BigNumber(base).shiftedBy(extra).toNumber()
-      if (base < 10 ** 16) {
-        return 0.00
-      }
-      return parseFloat(base).toFixed(2)
-    } else {
-      const data = new BigNumber(base).shiftedBy(-18).toString()
-      return parseFloat(data).toFixed(2)
-    }
+    return new BigNumber(base)
+      .shiftedBy(-18)
+      .precision(2, BigNumber.ROUND_FLOOR)
+      .toString()
   } catch (error) {
-    console.log(error)
+    return 'N/A'
   }
 }
